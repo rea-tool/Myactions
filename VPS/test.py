9545b070af77cb52f78d04f3d3673bb68c070135
rea-tool <105956707+rea-tool@users.noreply.github.com>
2022-09-05 00:29:12 +0800

    Create test.py

---
+++ VPS/test.py
@@ -0,0 +1,15 @@
+import os
+
+try:
+    USER_ID_H_02 = os.environ['USER_ID_H_02']
+except:
+    # 本地调试用
+    USER_ID_H_02 = ''
+    
+ try:
+    PASS_WD_H_02 = os.environ['PASS_WD_H_03']
+except:
+    # 本地调试用
+    PASS_WD_H_02 = ''
+  
+print('This is :' USER_ID_H_02)
